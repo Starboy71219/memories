71045a495663eb137709a13930381637a9e017cb
Ritesh Kumar <riteshkumar@Riteshs-MacBook-Air.local>
2023-08-18 20:07:19 +0530

    first commit

---
+++ client/build/precache-manifest.9246ca0d06b1770ceb9baf9e8647d50b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d88a5e3c7d8ac265eb4e3319f682247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6241df0204f0bc5939ef",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "d7166b4da54bb82bd25e",
+    "url": "/static/js/2.f1453214.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.f1453214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6241df0204f0bc5939ef",
+    "url": "/static/js/main.06635edf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.06635edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
